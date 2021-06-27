232c1c076dcdf8b94901b6791321f94dc9ffd3ee
Siemens Tutorials <55917384+siemenstutorials@users.noreply.github.com>
2021-06-27 11:24:59 +0900

    Update autov.sh

--- autov.sh
+++ autov.sh
@@ -71,6 +71,7 @@ Dockerkey=Lyb4534195
 #donload_docker-compose
 curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+cp -rf /usr/local/bin/docker-compose /usr/bin/docker-compose
 clear
 #config
 sed -i "s|SMT|v${Dockerid}|" docker-compose.yml
